920d3c7866fd8161c98b421099dbd29d60706aec
Aleksandr <kino_mac@mail.ru>
2022-02-07 16:07:17 +0300

    Добавлено решение 2-ой задачи

---
+++ Exercise2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти максимальное из трех чисел
+Console.Write("Введите 1-ое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3-е число: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if (numberB > max) {max = numberB;};
+if (numberC > max) {max = numberC;};
+Console.WriteLine(max);
