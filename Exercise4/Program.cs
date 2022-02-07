2a1854f2ebd925abe23fcd7fb641f9d3d2178b93
Aleksandr <kino_mac@mail.ru>
2022-02-07 17:27:07 +0300

    Добавлено решение 4-ой задачи

---
+++ Exercise4/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать четные числа от 1 до N
+
+Console.Write("Введите число N (N - целое число, больше 1) ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Четные числа от 1 до " + N + ":");
+for (int i = 1; i<= N; i++) {
+    if (i % 2 == 0) {Console.Write(i + "; ");};
+};
+
