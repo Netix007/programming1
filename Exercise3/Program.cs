9a5f9b0c14e5afe9676d1eec16ed16ce52346244
Aleksandr <kino_mac@mail.ru>
2022-02-07 16:35:53 +0300

    Добавлено решение 3-ей задачи

---
+++ Exercise3/Program.cs
@@ -0,0 +1,5 @@
+﻿// Выяснить является ли число чётным
+
+int number = new Random().Next(1,100);
+if (number % 2 == 0) {Console.Write("Число " + number + " - четное");}
+else {Console.Write("Число " + number + " - число нечетное");};
