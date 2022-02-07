9947b62e31244e25e1c7d0a219caa62591c5a92f
Aleksandr <kino_mac@mail.ru>
2022-02-07 15:02:03 +0300

    Добавлено решение 1-ой задачи

---
+++ Exercise1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Даны два числа. Показать большее и меньшее число.
+
+int numberA = new Random().Next(1,100);
+int numberB = new Random().Next(1,100);
+Console.WriteLine("Первое число: " + numberA);
+Console.WriteLine("Второе число: " + numberB);
+if (numberA == numberB)
+    {Console.WriteLine ("Числа равны");};
+if (numberA > numberB)
+    {Console.WriteLine("Большее число - " + numberA + "; " + "Меньшее число - " + numberB);}
+else {Console.WriteLine("Большее число - " + numberB + "; " + "Меньшее число - " + numberA);};
+    
